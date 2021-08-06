8f94f15128ccffde4f5796a29efd496733f92702
Frank <liangt2@rpi.edu>
2021-08-06 17:37:46 -0500

    another commit

--- preprocessing.py
+++ preprocessing.py
@@ -2,7 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os.path
 
-<<<<<<< HEAD
 data = pd.read_csv('New_York_State_Statewide_COVID-19_Testing.csv', 
     dtype={
         'New Positives': 'int',
@@ -24,17 +23,3 @@ for i in range(data.shape[0]):
         data.loc[i, '7-day Average Test Rate'] = sum(test_rates) / 14
 # print(data)
 data.to_csv('processed.csv')
-=======
-
-if (not os.path.isfile('testing.csv')):
-    data = pd.read_csv('New_York_State_Statewide_COVID-19_Testing.csv')
-    for i in range(data.shape[0]):
-        row = data.iloc[i]
-        if row['Test Date'][-1:] != '0' or row['Test Date'][:2] != '03' or row['Test Date'][3:5] >= '07':
-            positives = [int(data.iloc[i - x]['New Positives'].replace(',', '')) for x in range(7)]
-            data.loc[i, '7-day Average'] = int(sum(positives) / 7)
-    data.to_csv('testing.csv')
-else:
-    data = pd.read_csv('testing.csv')
-print(data[data['County'] == 'Albany'])
->>>>>>> 12da158b59bfc6bde0ca8c59ee5b9362497d599a
