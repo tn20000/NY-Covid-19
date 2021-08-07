eb5762fc9b7d5a5d7e842041bf343bab9a2a7dda
Frank <liangt2@rpi.edu>
2021-08-06 22:19:42 -0500

    trained all parameters

--- plotting.py
+++ plotting.py
@@ -1,50 +1,54 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from scipy import linalg
+
 def matrify(county_data):
     county_data = list(county_data)
     X = []
     for i in range(28,len(county_data)):
-        row = [1]
-        for j in range(14):
-            row.append(county_data[i-14+j])
+        row = [1] + county_data[i - 14:i]
         X.append(row)
     X = np.matrix(X)
     return X
 
 def lin_reg(county_data):
-    county_data = list(county_data)
-    X = matrify(county_data)
+    X = None
     Y = []
-    for i in range(28,len(county_data)):
-        Y.append([county_data[i]])
-    Y = np.matrix(Y)
-    Xi = np.linalg.inv((np.transpose(X)@X))@(np.transpose(X))
-    W = Xi@Y
-    index = list(data[data['County']=='Saratoga']['Test Date'])[28:]
-    predict = []
-    M = matrify(data[data['County']=='Saratoga']['7-day Average Positives'])
-    for i in range(28,len(county_data)):
-        predict.append(np.array(M[i-28]@W)[0][0])
-    plt.plot(index,predict)
+    for _, df in data.groupby('County'):
+        if X is None:
+            X = matrify(df['14-day Average Positives'])
+        else:
+            X = np.append(X, matrify(df['14-day Average Positives']), 0)
+        Y += [y for y in df['14-day Average Positives'][28:]]
+    X = np.array(X)
+    Y = np.array(Y)
+    # Y = np.matrix(Y)
+    # Xi = np.linalg.inv((np.transpose(X)@X))@(np.transpose(X))
+    # W = Xi@Y
+    W = linalg.lstsq(X, Y)[0]
+    return W
+
+def predict(W, county):
+    true = list(data[data['County'] == county]['14-day Average Positives'])[28:]
+    county_data = matrify(data[data['County'] == county]['14-day Average Positives'])
+    pred = np.array(county_data @ W)[0]
+    index = data[data['County'] == county]['Test Date'][28:]
+    plt.plot(index, pred)
+    plt.plot(index, true)
     plt.show()
-    print(W)
+    diff = np.array(true) - pred
+    plt.plot(index, diff)
+    plt.show()
+    error = np.linalg.norm(diff) / len(diff)
+    print(error)
 
 data = pd.read_csv('processed.csv',
     dtype={
         'New Positives': 'int',
         'Cumulative Number of Positives': 'int',
         'Total Number of Tests Performed': 'int',
-        'Cumulative Number of Tests Performed': 'int'}, parse_dates=['Test Date'], thousands=',')
-county = input('Enter the county\n')
-xlabel = input('Enter the x axis\n')
-ylabel = input('Enter the y axis\n')
-county_data = data[data['County']==county][ylabel]
-plt.plot(list(data[data['County']==county][xlabel]),county_data)
-max_window = plt.get_current_fig_manager()
-max_window.window.state('zoomed')
-plt.show()
-lin_reg(county_data)
-
-
-
+        'Cumulative Number of Tests Performed': 'int'}, parse_dates=['Test Date'])
+W = lin_reg(data)
+print(W.shape)
+predict(W, 'New York')
