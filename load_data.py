ae0fd3666910ee7e359b420a13b017f95e7811a4
Frank <liangt2@rpi.edu>
2021-08-07 19:34:04 -0500

    another commit

--- load_data.py
+++ load_data.py
@@ -1,6 +1,6 @@
 import wget
 
 class DataGrabber:
-    def load_data():
+    def load_data(self):
         u = 'https://health.data.ny.gov/api/views/xdss-u53e/rows.csv?accessType=DOWNLOAD'
         f = wget.download(u)
